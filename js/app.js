e51c56a426717926c3fead2d3df6c2a0493ec7ea
Faber Vergara <134828+emamut@users.noreply.github.com>
2023-04-29 10:29:38 -0500

    add map gesture handling

--- js/app.js
+++ js/app.js
@@ -1,16 +1,9 @@
-var map = L.map('map', { scrollWheelZoom: false }).setView(
-  [-0.20569, -78.4822],
-  16
-);
-
-map.on('click', function () {
-  if (map.scrollWheelZoom.enabled()) {
-    map.scrollWheelZoom.disable();
-  } else {
-    map.scrollWheelZoom.enable();
-  }
-});
+var map = L.map('map', { gestureHandling: true }).setView([51.505, -0.09], 13);
 
 L.tileLayer.provider('Stadia.OSMBright').addTo(map);
-
 var marker = L.marker([-0.20569, -78.4822]).addTo(map);
+
+var latLngs = [marker.getLatLng()];
+var markerBounds = L.latLngBounds(latLngs);
+
+map.fitBounds(markerBounds);
